61875026dff3a965f270dd434b8a418aa35ab2d0
hariDasu <harirao3@gmail.com>
2014-09-29 18:28:42 -0400

    removing unused code

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var curl = require('node-curl');
 
 
 
-app.get('/scrape', function(req, res){
+app.get('/crawl', function(req, res){
 
 url = 'http://www.alise.org/alise-membership---2014---institutional-members';
 
@@ -26,26 +26,15 @@ request(url, function(error, response, html){
             var links = [];
 
             data.find('a').each(function () {
-
-                //console.log(typeof linkName);
-                //console.log(this.attribs.href);
                 var actualLink = String(this.attribs.href)
-                //console.log(typeof actualLink);
                 if (typeof actualLink === 'string') {
-                    //console.log(actualLink);
                     links.push(actualLink);
-
-
                 }
-
-
             });
             links.splice(0, 1);
 
             var absLinks = [];
             links.forEach(function (actualLink, index, allItems) {
-                //console.log(typeof actualLink);
-
                 if ((actualLink.indexOf("http:")) != -1) {
                     absLinks.push(actualLink);
                 }
@@ -53,24 +42,13 @@ request(url, function(error, response, html){
 
 
             var uniqueLinks = _.uniq(absLinks);
-            console.log(uniqueLinks[0]);
+            console.log(uniqueLinks);
 
-            /*async.each(uniqueLinks, request(uniqueLinks,function(error,response,body){
-                if (!error && response.statusCode == 200) {
-                    console.log(body)
-                }
-            }), function (err){
-                if(!err){
-                    console.log('yay')
-                }else(console.log(err));
-            });*/
             for (i = 0; i < uniqueLinks.length; i++) {
                 request(uniqueLinks[i]).pipe(fs.createWriteStream('page'+i+'.html'));
 
             };
         })
-
-
     }
 
         res.
