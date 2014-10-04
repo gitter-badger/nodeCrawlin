bd1e64ddf04491f474b5ecd204c45dff0ed90fa4
hariDasu <harirao3@gmail.com>
2014-10-04 17:16:46 -0400

    broken now

--- konsume.js
+++ konsume.js
@@ -19,62 +19,63 @@ console.log('Magic happens on port 8081');
 
 exports = module.exports = app;
 app.get('/crawl', function(req, res){
-    jobs.process('url',function(job,done){
-        console.log(job.data)
 
-            setTimeout(function(){
-                url = job.data.url;
-                breadcrumb=job.data.breadcrumb;
-                level=job.data.level;
-
-                request(url, function(error, response, html){
-                    if (error) {
-                    } else {
-                        var $ = cheerio.load(html);
-
-                        $('body').filter(function () {
-                            var data = $(this);
-
-                            var links = [];
-
-                            data.find('a').each(function () {
-                                var actualLink = String(this.attribs.href)
-                                if (typeof actualLink === 'string') {
-                                    links.push(actualLink);
-                                }
-                            });
-                            links.splice(0, 1);
-
-                            var absLinks = [];
-                            links.forEach(function (actualLink, index, allItems) {
-                                if ((actualLink.indexOf("http:")) != -1) {
-                                    absLinks.push(actualLink);
-                                }
-                            });
+jobs.process('url',function(job,done){
+        console.log(job.data);
 
 
-                            var uniqueLinks = _.uniq(absLinks);
-                            //console.log(uniqueLinks);
-                            var i = 0;
+        url = job.data.url;
+        breadcrumb=job.data.breadcrumb;
+        level=job.data.level;
+
+        request(url, function(error, response, html){
+            if (error) {
+            } else {
+                var $ = cheerio.load(html);
+
+                $('body').filter(function () {
+                    var data = $(this);
+
+                    var links = [];
+
+                    data.find('a').each(function () {
+                        var actualLink = String(this.attribs.href)
+                        if (typeof actualLink === 'string') {
+                            links.push(actualLink);
+                        }
+                    });
+                    links.splice(0, 1);
+
+                    var absLinks = [];
+                    links.forEach(function (actualLink, index, allItems) {
+                        if ((actualLink.indexOf("http:")) != -1) {
+                            absLinks.push(actualLink);
+                        }
+                    });
 
 
-                            /*for (i = 0; i < uniqueLinks.length; i++) {
-                                //request(uniqueLinks[i]).pipe(fs.createWriteStream('page'+i+'.html'));
-                            };*/
-                            console.log(uniqueLinks);
-                            var returnResults = {
-                                foundUrls:uniqueLinks,
-                                breadcrumb:breadcrumb,
-                                level:level
-                            };
-                            done(null,returnResults)
+                    var uniqueLinks = _.uniq(absLinks);
+                    //console.log(uniqueLinks);
+                    var i = 0;
 
-                        })
-                    }
-                    res.send('Check your console!')
+
+                    /*for (i = 0; i < uniqueLinks.length; i++) {
+                        //request(uniqueLinks[i]).pipe(fs.createWriteStream('page'+i+'.html'));
+                    };*/
+                    console.log(uniqueLinks);
+                    var returnResults = {
+                        foundUrls:uniqueLinks,
+                        breadcrumb:breadcrumb,
+                        level:level
+                    };
+                    done(null,returnResults)
+
                 })
-
-            },20000)
+            }
+            res.send('Check your console!')
         })
+
+
+    });
 })
 
--- kueTest.js
+++ kueTest.js
@@ -11,53 +11,6 @@ var _ = require('underscore');
 var kue = require('kue'),
     jobs=kue.createQueue();
 
-var sequence = 0;
-
-/*setInterval(
-    function() {
-        sequence +=1;
-        (function(sequence){
-
-            var job = jobs.create('email',{
-                title:'Hello # ' + sequence
-                ,to: 'harirao3@gmail.com'
-                ,body: 'hello from kue!'
-            }).save();
-
-            job.on('complete', function(){
-                console.log('job '+ sequence +' completed');
-            });
-            job.on('failed', function(){
-                console.log('job '+ sequence +'  failed');
-            });
-        })(sequence)
-    }
-    ,1000
-);*/
-
-/*
-
-nextCrawl=function(level,urls){
-    urls.forEach(function(oneUrl, index,urls){
-        console.log("urls : ", oneUrl , " added to queue for crawling " ,level ,"deep");
-
-    });
-}
-
-
-var job = jobs.create('url', {
-    url: 'http://www.alise.org/alise-membership---2014---institutional-members'
-    ,level: 1
-}).save();
-
-job.on('complete', function (result) {
-    console.log('job ' + sequence + ' results: ', result);
-
-});
-job.on('failed', function () {
-    console.log('job ' + sequence + '  failed');
-});
-*/
 
 var seedUrl=['http://www.alise.org/alise-membership---2014---institutional-members'];
 var crawlUrls= function (level,urlsToCrawl,breadcrumb) {
@@ -66,7 +19,11 @@ var crawlUrls= function (level,urlsToCrawl,breadcrumb) {
 
         urlsToCrawl.forEach(function(oneUrl,index)
         {
-            var bCrumb=breadcrumb+'|'+oneUrl;
+            var bCrumb="seedUrl";
+            if(breadcrumb!=seedUrl){
+                bCrumb=breadcrumb+'|'+oneUrl;
+            }
+
             var crawlJob = jobs.create('url', {
                 url: oneUrl
                ,level:level+1
@@ -74,11 +31,12 @@ var crawlUrls= function (level,urlsToCrawl,breadcrumb) {
             }).save();
             myJobs.push(crawlJob)
         });
-        var nextUrls = [];
+    console.log(myJobs);
         myJobs.forEach(function(job,index)
             {
-              nextUrls.push()
+
               job.on('complete',function(result){
+                  console.log(result);
                   if (result.level<2){
                       console.log('cb level: '+(result.level+1))
                       crawlUrls(result.level,result.foundUrls,result.breadcrumb)
